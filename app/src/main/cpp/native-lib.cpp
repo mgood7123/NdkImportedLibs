44d72878e77eadd10ebc4c850ae60b8ef4993f32
Matthew Good <smallville7123@gmail.com>
2020-04-22 22:04:46 +1000

    LinkError: dlopen failed: library "libavcodec.so" not found

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,19 @@
 #include <jni.h>
 #include <string>
+#include <dlfcn.h>
+#include "OboeDebug.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_general_code_jnilibsprebuilt_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+//    LOGV("attempting to open %s", "avcodec");
+//    void * avcodec = dlopen("avcodec", RTLD_NOW);
+//    if (avcodec == nullptr) {
+//        LOGV("%s could not be opened: %s", "avcodec", dlerror());
+//    } else {
+//        LOGV("%s opened successfully", "avcodec");
+//    }
     return env->NewStringUTF(hello.c_str());
 }
---
+++ install_debug_unsigned_apk.sh
@@ -0,0 +1,1 @@
+adb uninstall general.code.jnilibsprebuilt ; adb install APKs/app-debug-unsigned.apk
